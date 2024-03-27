804caa70f5c6e1e619ed258f298618436b69a05b
Sakiynat10 <xoliyorvasilbek4@gmail.com>
2024-03-28 02:53:26 +0500

    make available madia links

--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -36,13 +36,13 @@ export class Hero extends Component {
                   <button>{t.heroBtn}</button>
                 </div>
                 <div className="hero-medias">
-                  <a href="#a">
+                  <a href="https://www.bing.com/ck/a?!&&p=bbed671e772e03ccJmltdHM9MTcxMTQ5NzYwMCZpZ3VpZD0wNTdjMGZmZC02ZmYyLTY3ZTQtM2I3YS0xYzNkNmVmNDY2YmMmaW5zaWQ9NTI0NA&ptn=3&ver=2&hsh=3&fclid=057c0ffd-6ff2-67e4-3b7a-1c3d6ef466bc&psq=%40asilbek_xoliyorov&u=a1aHR0cHM6Ly93d3cuZmFjZWJvb2suY29tL3B1YmxpYy9Bc2lsYmVrLVhvbGl5b3Jvdi8&ntb=1">
                     <img src="/vk.svg" alt="wk" />
                   </a>
-                  <a href="#a">
+                  <a href="https://r.search.yahoo.com/_ylt=AwrFeUsWlARmLQQAEKhXNyoA;_ylu=Y29sbwNiZjEEcG9zAzEEdnRpZAMEc2VjA3Ny/RV=2/RE=1712785687/RO=10/RU=https%3a%2f%2fwww.instagram.com%2fasilbekxoliyorov%2f/RK=2/RS=UrBpR.5ccGR6O4_BCx6OJZEhVUI-">
                     <img src="/fb.svg" alt="fb" />
                   </a>
-                  <a href="#a">
+                  <a href="https://www.linkedin.com/in/asilbek-xoliyorov-58127a2aa/?lipi=urn%3Ali%3Apage%3Ad_flagship3_feed%3B8%2FKkwuctRE6B4rubZv2sew%3D%3D">
                     <img src="/ln.svg" alt="ln" />
                   </a>
                 </div>
@@ -61,13 +61,13 @@ export class Hero extends Component {
                   <button>{t.heroBtn}</button>
                 </div>
                 <div className="hero-medias">
-                  <a href="#a">
+                  <a href="https://www.bing.com/ck/a?!&&p=bbed671e772e03ccJmltdHM9MTcxMTQ5NzYwMCZpZ3VpZD0wNTdjMGZmZC02ZmYyLTY3ZTQtM2I3YS0xYzNkNmVmNDY2YmMmaW5zaWQ9NTI0NA&ptn=3&ver=2&hsh=3&fclid=057c0ffd-6ff2-67e4-3b7a-1c3d6ef466bc&psq=%40asilbek_xoliyorov&u=a1aHR0cHM6Ly93d3cuZmFjZWJvb2suY29tL3B1YmxpYy9Bc2lsYmVrLVhvbGl5b3Jvdi8&ntb=1">
                     <img src="/vk.svg" alt="wk" />
                   </a>
-                  <a href="#a">
+                  <a href="https://r.search.yahoo.com/_ylt=AwrFeUsWlARmLQQAEKhXNyoA;_ylu=Y29sbwNiZjEEcG9zAzEEdnRpZAMEc2VjA3Ny/RV=2/RE=1712785687/RO=10/RU=https%3a%2f%2fwww.instagram.com%2fasilbekxoliyorov%2f/RK=2/RS=UrBpR.5ccGR6O4_BCx6OJZEhVUI-">
                     <img src="/fb.svg" alt="fb" />
                   </a>
-                  <a href="#a">
+                  <a href="https://www.linkedin.com/in/asilbek-xoliyorov-58127a2aa/?lipi=urn%3Ali%3Apage%3Ad_flagship3_feed%3B8%2FKkwuctRE6B4rubZv2sew%3D%3D">
                     <img src="/ln.svg" alt="ln" />
                   </a>
                 </div>
@@ -86,13 +86,13 @@ export class Hero extends Component {
                   <button>{t.heroBtn}</button>
                 </div>
                 <div className="hero-medias">
-                  <a href="#a">
+                  <a href="https://www.bing.com/ck/a?!&&p=bbed671e772e03ccJmltdHM9MTcxMTQ5NzYwMCZpZ3VpZD0wNTdjMGZmZC02ZmYyLTY3ZTQtM2I3YS0xYzNkNmVmNDY2YmMmaW5zaWQ9NTI0NA&ptn=3&ver=2&hsh=3&fclid=057c0ffd-6ff2-67e4-3b7a-1c3d6ef466bc&psq=%40asilbek_xoliyorov&u=a1aHR0cHM6Ly93d3cuZmFjZWJvb2suY29tL3B1YmxpYy9Bc2lsYmVrLVhvbGl5b3Jvdi8&ntb=1">
                     <img src="/vk.svg" alt="wk" />
                   </a>
-                  <a href="#a">
+                  <a href="https://r.search.yahoo.com/_ylt=AwrFeUsWlARmLQQAEKhXNyoA;_ylu=Y29sbwNiZjEEcG9zAzEEdnRpZAMEc2VjA3Ny/RV=2/RE=1712785687/RO=10/RU=https%3a%2f%2fwww.instagram.com%2fasilbekxoliyorov%2f/RK=2/RS=UrBpR.5ccGR6O4_BCx6OJZEhVUI-">
                     <img src="/fb.svg" alt="fb" />
                   </a>
-                  <a href="#a">
+                  <a href="https://www.linkedin.com/in/asilbek-xoliyorov-58127a2aa/?lipi=urn%3Ali%3Apage%3Ad_flagship3_feed%3B8%2FKkwuctRE6B4rubZv2sew%3D%3D">
                     <img src="/ln.svg" alt="ln" />
                   </a>
                 </div>
